3be32cb452ad475f1cc47dba09017c70a8b83f06
joeljoby111 <joeljoby111@gmail.com>
2024-07-18 09:56:13 +0530

    fix : department fetching in edit employee

--- react-app/src/Pages/EditEmployee/EditEmployee.jsx
+++ react-app/src/Pages/EditEmployee/EditEmployee.jsx
@@ -3,6 +3,7 @@ import Form from "../../components/Form";
 import { useEffect } from "react";
 import { useEditEmployeeMutation } from "../EmployeeList/api";
 import { useGetEmployeeDetailsQuery } from "../EmployeeList/api";
+import { useGetDepartmentListQuery } from "../EmployeeList/department.api";
 const deptOptionList = [
   {
     value: "select",
@@ -136,6 +137,7 @@ const EditEmployee = () => {
 
   const { id } = useParams();
   const { data = {}, isError, isSuccess } = useGetEmployeeDetailsQuery(id);
+
   console.log("the id is", id);
   const getEmployee = () => {
     if (isSuccess) {
@@ -158,6 +160,76 @@ const EditEmployee = () => {
     }
     return {};
   };
+
+  const { data: DeptData = [] } = useGetDepartmentListQuery();
+  const Fields = [
+    {
+      id: "empname",
+      inputPlaceholder: "Employee name",
+      labelContent: "Employee name",
+      name: "Employee name",
+      type: "text",
+    },
+    {
+      id: "eid",
+      inputPlaceholder: "Employee ID",
+      labelContent: "Employee ID",
+      name: "Employee ID",
+      type: "text",
+    },
+    {
+      id: "jdate",
+      inputPlaceholder: "Joining Date",
+      labelContent: "Joining Date",
+      name: "Joining Date",
+      type: "date",
+    },
+    {
+      id: "crole",
+      labelContent: "Choose Role",
+      name: "Choose Role",
+      optionList: croleOptionList,
+    },
+    {
+      id: "status",
+      labelContent: "Status",
+      name: "Status",
+      optionList: statusOptionList,
+    },
+    {
+      id: "exp",
+      inputPlaceholder: "Experience",
+      labelContent: "Experience",
+      name: "Experience",
+      type: "number",
+    },
+    {
+      id: "address1",
+      inputPlaceholder: "Address",
+      labelContent: "Address Line1",
+      name: "Address Line 1",
+      type: "text",
+    },
+    {
+      id: "address2",
+      inputPlaceholder: "Address",
+      labelContent: "Address Line 2",
+      name: "Address Line 2",
+      type: "text",
+    },
+    {
+      id: "dept",
+      labelContent: "Department",
+      name: "Department",
+      optionList: [
+        ...[{ value: "status", content: "status" }],
+        ...DeptData.map((element) => ({
+          value: element.name,
+          content: element.name,
+        })),
+      ],
+    },
+  ];
   return (
     <main>
       <section className="sec1">
